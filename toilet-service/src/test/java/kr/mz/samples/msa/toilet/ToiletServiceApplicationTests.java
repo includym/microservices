127587222bf91275286ececbcc7ce2e6465c928d
includym <hong@mz.co.kr>
2016-07-28 15:10:29 +0900

    init toilet-service

---
+++ toilet-service/src/test/java/kr/mz/samples/msa/toilet/ToiletServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.mz.samples.msa.toilet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ToiletServiceApplication.class)
+public class ToiletServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
