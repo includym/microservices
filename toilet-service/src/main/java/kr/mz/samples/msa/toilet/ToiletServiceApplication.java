0ac4837f7aea3010d9c7f7497ee26ebfd192570b
includym <hong@mz.co.kr>
2016-07-28 16:16:41 +0900

    remove @RefreshScope

--- toilet-service/src/main/java/kr/mz/samples/msa/toilet/ToiletServiceApplication.java
+++ toilet-service/src/main/java/kr/mz/samples/msa/toilet/ToiletServiceApplication.java
@@ -2,7 +2,6 @@ package kr.mz.samples.msa.toilet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +17,6 @@ public class ToiletServiceApplication {
 
 }
 
-@RefreshScope
 @RestController
 @RequestMapping(value="/toilets")
 class AccountController {
